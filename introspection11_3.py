51b7c9d970417d587acb05ce21e39beec6e73357
Lena-vetochka <elenanechaeva1701@yandex.ru>
2024-11-24 19:04:03 +0300

    Add files via upload
    Необходимо создать функцию, которая принимает объект (любого типа) в качестве аргумента и проводит интроспекцию этого объекта, чтобы определить его тип, атрибуты, методы, модуль, и другие свойства.

---
+++ introspection11_3.py
@@ -0,0 +1,32 @@
+import inspect
+from pprint import pprint
+
+
+def introspection_info(obj):
+    obj_type = type(obj).__name__
+
+    # Получаем атрибуты объекта
+    attributes = [attr for attr in dir(obj) if not callable(getattr(obj, attr)) and not attr.startswith("__")]
+
+    # Получаем методы объекта
+    methods = [method for method in dir(obj) if callable(getattr(obj, method)) and not method.startswith("__")]
+
+    #Получаем модуль, к которому принадлежит объект
+    obj_module = obj.__class__.__module__
+
+    info_obj = {
+        'type': obj_type,
+        'attributes': attributes,
+        'methods': methods,
+        'module': obj_module
+    }
+
+    return info_obj
+
+
+number_info = introspection_info(42)
+#print(number_info)
+pprint(number_info, width = 1, sort_dicts= False)
+
+
+
